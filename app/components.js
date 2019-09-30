12b8fcf6887cbe2c2138f754a151f35c6a147c32
paul-pdftron <42223337+paul-pdftron@users.noreply.github.com>
2019-09-30 11:57:26 -0700

    comment out license key property

--- app/components.js
+++ app/components.js
@@ -8,7 +8,7 @@ angular.module('components', [])
       controller: function($scope, $element) {
         WebViewer({
           path: 'lib', // path to the PDFTron 'lib' folder on your server
-          licenseKey: 'Insert commercial license key here after purchase',
+          // licenseKey: 'Insert commercial license key here after purchase',
           initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
           // initialDoc: '/path/to/my/file.pdf',  // You can also use documents on your server
         }, document.getElementById('viewer'))
